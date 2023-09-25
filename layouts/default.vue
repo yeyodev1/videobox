ca840f7e1b02ac17c7ad0f2cfc77b3136b685c05
Diego Reyes <diegorele13@gmail.com>
2023-09-25 17:57:46 -0500

    set: header & footer web

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,12 +2,20 @@
 import TheHeader from '@/layouts/web/TheHeader.vue'
 import TheMenu from './web/TheMenu.vue';
 import TheFooter from './web/TheFooter.vue';
+
+const showWebMenu = ref(false);
+
+const toggleShowMenu = () => {
+  showWebMenu.value = !showWebMenu.value;
+};
 </script>
 
 <template>
   <div class="app-container">
-    <TheHeader />
-    <TheMenu />
+    <TheHeader @toggle-menu="toggleShowMenu"/>
+    <TheMenu
+      :isVisible="showWebMenu"  
+      @toggle-menu="toggleShowMenu"/>
     <div class="app-container-view">
       <slot  />
     </div>
