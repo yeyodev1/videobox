6c94d5ef5337c65a7719c25b416b6272bea1844b
Diego Reyes <diegorele13@gmail.com>
2023-09-26 18:09:59 -0500

    set: principal home

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,49 @@
 <script setup lang="ts">
-import MainContent from '@/components/Home/MainContent.vue';
+import useFieldStore from '@/store/FieldStore'
+
+
+const fieldStore = useFieldStore();
 </script>
 
 <template>
-  <div>
-    <MainContent/>
+  <div class="container">
+    <h2 class="container-title">
+      Disfruta de tus mejores jugadas en HD
+    </h2>
+    <p class="container-question">
+      ¿En dónde jugaste?
+    </p>
+    <div class="container-cards">
+      <FieldSportCard
+        v-for="(field, index) in fieldStore.fields"
+        :key="index"
+        :name="field.name"
+        :image="field.image"
+        :id="field.id"/>
+    </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.container {
+  padding: 24px;
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+  &-title {
+    font-size: $h1-font-size;
+    text-align: center;
+  }
+  &-question {
+    font-size: $h2-font-size;
+    text-align: center;
+  }
+  &-cards {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    gap: 24px;
+  }
+}
+</style>
