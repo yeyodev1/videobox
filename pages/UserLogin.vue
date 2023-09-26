5a0c6c1abffecf88b16ddd06b5bf8390ea8da223
Diego Reyes <diegorele13@gmail.com>
2023-09-25 19:41:41 -0500

    done: login page

---
+++ pages/UserLogin.vue
@@ -0,0 +1,163 @@
+<script setup lang="ts">
+import { computed, reactive, ref } from 'vue';
+
+import CrushTextField from '@nabux-crush/crush-text-field';
+import CrushButton from '@nabux-crush/crush-button';
+
+import { validateEmail } from '@/utils/AuthValidations';
+
+
+// it forces to re-render the component once the value change
+const textKey = ref(0);
+const isPasswordVisible = ref(false);
+const userData = reactive({
+  email: '',
+  password: '',
+});
+const userRules = {
+  emailValidation: [
+    {
+      validate: (value: string) => validateEmail(value),
+      message: 'Ingresa un correo válido' 
+    },
+    {
+      validate: (value: string) => value.length,
+      message: 'Ingresa un correo'
+    }
+  ],
+  passwordValidation: [
+    {
+      validate: (value: string) => value.length > 7,
+      message: 'El password debe tener al menos 10 caracteres' 
+    }
+  ]
+}
+const enableForm = computed(() => {
+  return userData.email !== '' &&
+    userData.password !== '' &&
+    userRules.emailValidation.every((rule) => rule.validate(userData.email)) &&
+    userRules.passwordValidation.every((rule) => rule.validate(userData.password))
+});
+const passwordIcon = computed(() => {
+  return isPasswordVisible.value ? 'fa-solid fa-eye-slash' : 'fa-solid fa-eye';
+});
+const textType = computed(() => {
+  return isPasswordVisible.value ? 'text' : 'password';
+});
+
+
+function handleLogin(): void {
+  console.log('nos estamos logeando')
+}
+
+function resetValue(): void {
+  userData.email = '';
+  userData.password = '';
+  textKey.value ++
+}
+
+</script>
+
+<template>
+  <div class="login-wrapper crush-container">
+    <p class="login-wrapper-title">
+      Que gusto verte nuevamente 🌟 <br> Ingresa ahora
+    </p>
+    <div class="login-wrapper-card">
+      <CrushTextField
+        :key="textKey"
+        v-model="userData.email"
+        label="Correo"
+        :validRules="userRules.emailValidation" />
+      <CrushTextField
+        :key="textKey"
+        v-model.trim="userData.password"
+        label="Contraseña"
+        :type="textType"
+        :validRules="userRules.passwordValidation">
+        <template #icon>
+          <button
+            class="icon-button"
+            @click="isPasswordVisible = !isPasswordVisible">
+            <i :class="passwordIcon" />
+          </button>
+        </template>
+      </CrushTextField>
+      <CrushButton
+        variant="primary"
+        text="Inicia Sesión"
+        :disabled="!enableForm"
+        @click.prevent="handleLogin" />
+    </div>
+    <span class="login-wrapper-span">
+      ¿Aún no tienes cuenta? 
+      <RouterLink 
+        class="login-wrapper-span-link"
+        to="/register">
+        Regístrate ahora
+      </RouterLink> 
+    </span>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+:deep(.calendar-input) {
+  color: $white;
+}
+.login-wrapper {
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 16px;
+  align-items: center;
+  min-height: 100vh;
+  padding: 24px;
+  &-title {
+    color: $white;
+    font-size: $body-font-size;
+    text-align: center;
+    @media (min-width: $tablet-lower-breakpoint) {
+      font-size: $h3-font-size;
+    }
+  }
+  .date-message {
+    color: $red;
+  }
+  &-card {
+    border: 1px solid $grey;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    width: 100%;
+    max-width: 520px;
+    border-radius: 8px;
+    padding: 32px;
+    background: rgba(141, 141, 157, 0.08);
+    backdrop-filter: blur(40px);
+    @media(max-width: $tablet-lower-breakpoint) {
+      border: none;
+    }
+    .hoot-text-field {
+      :first-child {
+        margin-bottom: 8px;
+      }
+    }
+    .icon-button {
+      background-color: transparent;
+      color: $white;
+      border: none;
+    }
+    .crush-button {
+      margin-top: 12px;
+    }
+  }
+  &-span {
+    color: $white;
+    &-link {
+      color: $purple;
+      text-decoration: underline;
+    }
+  }
+}
+</style>
