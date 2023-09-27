8e449abb9382afacde8dcb87c9ad64633e404138
Diego Reyes <diegorele13@gmail.com>
2023-09-27 17:42:22 -0500

    export type

--- typings/Field&Sport.ts
+++ typings/Field&Sport.ts
@@ -1,16 +1,24 @@
+interface Schedule {
+  startHour: string,
+  endHour: string 
+}
+
 interface Sport {
   image: string,
   name: string,
   id: string,
+  schedule: Schedule[]
 }
+
 
 interface Field {
   image: string,
   name: string,
   id: string,
-  sports: Sport []
+  sports: Sport[]
 }
 
 export type {
   Field,
+  Sport
 }
