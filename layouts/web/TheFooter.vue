462ede12cdf0b6809cfe817bd3a5ec7924b08992
Diego Reyes <diegorele13@gmail.com>
2023-09-27 18:59:01 -0500

    done: footer

--- layouts/web/TheFooter.vue
+++ layouts/web/TheFooter.vue
@@ -1,7 +1,74 @@
+<script setup lang="ts">
+function getYear(): number {
+  const date = new Date();
+  const currentYear = date.getFullYear();
+  return currentYear;
+}
+</script>
+
 <template>
-  <div>
-    <p>
-      este es el footer
-    </p>
-  </div>
+  <footer class="footer">
+    <div class="footer-container">
+      <span>
+        <i class="fa-brands fa-facebook"></i>
+      </span>
+      <span>
+        <i class="fa-brands fa-instagram"></i>
+      </span>
+    </div>
+    <div class="yeyodev">
+      <div class="yeyodev-details">
+        <p>Copyright</p>
+        <i class="fa-regular fa-copyright" />
+        <p class="yeyodev-details-author">
+          <a href="https://www.yeyo.dev/" target="_blank">YeyoDev <i class="fa-solid fa-heart" /> {{ getYear() }}</a> - Todos los derechos reservados
+        </p>
+      </div>
+    </div>
+  </footer>
 </template>
+
+<style lang="scss" scoped>
+.footer {
+  padding: 24px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  &-container {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 12px;
+    span {
+      color: $white;
+      font-size: $h3-font-size;
+    }
+  }
+}
+.yeyodev {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  &-details {
+    color: $white;
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+    p {
+      font-size: 1rem;
+      a {
+        font-size: 1rem;
+        color: $white;
+        text-decoration: none;
+        i {
+          color: #DA87B1;
+        }
+      }
+    }
+    }
+  }
+</style>
