06c50ef8a502b23e36d5db060054caef72819f67
Diego Reyes <diegorele13@gmail.com>
2023-09-26 19:26:59 -0500

    done: club page

--- pages/[club].vue
+++ pages/[club].vue
@@ -1,13 +1,35 @@
 <script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import { useRoute } from 'vue-router';
+import useFieldStore from '@/store/FieldStore';
+import type { Field } from '~/typings/Field&Sport';
+
+const fieldStore = useFieldStore();
+const route = useRoute();
+const fieldSelected = ref<Field | null>(null);
+
+onMounted(() => {
+  const fieldId = route.params.club
+  fieldSelected.value = fieldStore.fields.find((field: Field) => field.id === fieldId) ?? null;
+});
+const fieldId = route.params.club
 
 
 </script>
 
 <template>
-  <div class="container">
-    <p>
-      aqui tienen que ir los deportes de cada complejo
-    </p>
+  <div 
+    v-if="fieldSelected"
+    class="container" >
+    <h2 class="container-title">
+      Estas en la seccion de {{ fieldSelected.name }}
+    </h2>
+    <div class="container-cards">
+      <FieldSportCard
+        :name="fieldSelected.sports[0].name"
+        :image="fieldSelected.sports[0].image"
+        :id="`${fieldId}/${fieldSelected.sports[0].id}/schedule`" />
+    </div>
   </div>
 </template>
 
@@ -15,8 +37,15 @@
 .container {
   display: flex;
   justify-content: center;
+  flex-direction: column;
   align-items: center;
   flex-wrap: wrap;
   gap: 24px;
+  &-cards {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+  }
 }
 </style>
