82bb7cafa73a9d18e22d5ee1d2cd91427058a075
Diego Reyes <diegorele13@gmail.com>
2023-09-26 19:27:33 -0500

    fix: store

--- store/FieldStore.ts
+++ store/FieldStore.ts
@@ -13,13 +13,11 @@ const useFieldStore = defineStore('useFieldStore', {
         name: 'complejo #1',
         image: 'https://i.pinimg.com/736x/43/83/47/438347c76787a041cf4ce69f86790d5f.jpg',
         id: 'complejo-1',
-        link: 'complejo-1',
         sports: [
           {
             name: 'tenis',
             image: 'https://i.pinimg.com/736x/f6/52/12/f65212658964e531f7d6f4a4a2331712.jpg',
             id: 'tenis',
-            link: 'tenis-1'
           },
         ]
       },
@@ -27,13 +25,11 @@ const useFieldStore = defineStore('useFieldStore', {
         name: 'Complejo 2',
         image: 'https://i.pinimg.com/736x/b1/c4/af/b1c4afe4e893582ffbda3c48ccca6c81.jpg',
         id: 'complejo-2',
-        link: 'complejo-2',
         sports: [
           {
             name: 'tenis',
             image: 'https://i.pinimg.com/736x/f6/52/12/f65212658964e531f7d6f4a4a2331712.jpg',
-            id: 'tenis',
-            link: 'tenis-2'
+            id: 'tenis-2',
           },
         ]
       },
@@ -41,13 +37,11 @@ const useFieldStore = defineStore('useFieldStore', {
         name: 'Complejo 3',
         image: 'https://i.pinimg.com/736x/b1/c4/af/b1c4afe4e893582ffbda3c48ccca6c81.jpg',
         id: 'Complejo-3',
-        link: 'Complejo-3',
         sports: [
           {
             name: 'tenis',
             image: 'https://i.pinimg.com/736x/f6/52/12/f65212658964e531f7d6f4a4a2331712.jpg',
-            id: 'tenis',
-            link: 'tenis-3'
+            id: 'tenis-3',
           },
         ]
       },
@@ -55,13 +49,11 @@ const useFieldStore = defineStore('useFieldStore', {
         name: 'Complejo 4',
         image: 'https://i.pinimg.com/736x/b1/c4/af/b1c4afe4e893582ffbda3c48ccca6c81.jpg',
         id: 'Complejo-4',
-        link: 'Complejo-4',
         sports: [
           {
             name: 'tenis',
             image: 'https://i.pinimg.com/736x/f6/52/12/f65212658964e531f7d6f4a4a2331712.jpg',
-            id: 'tenis',
-            link: 'tenis-4'
+            id: 'tenis-4',
           },
         ]
       },
