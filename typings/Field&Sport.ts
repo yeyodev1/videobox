53eb3b31cf4dceb0626fe5d28b1d94b0b388b03d
Diego Reyes <diegorele13@gmail.com>
2023-09-26 19:26:38 -0500

    rm: unnecesary type

--- typings/Field&Sport.ts
+++ typings/Field&Sport.ts
@@ -2,14 +2,12 @@ interface Sport {
   image: string,
   name: string,
   id: string,
-  link: string,
 }
 
 interface Field {
   image: string,
   name: string,
   id: string,
-  link: string,
   sports: Sport []
 }
 
