c0e7b565029460e958eafd088e77b4c664a52981
Diego Reyes <diegorele13@gmail.com>
2023-10-10 18:19:36 -0500

    done: purchase page

---
+++ pages/Purchase.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="section">
+    <p class="section-title">
+      Envía tu pago mediante el siguiente código QR
+    </p>
+    <img 
+      src="@/assets/qr-code.jpg" 
+      alt="Código de pago" 
+      class="section-image">
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.section {
+  padding: 24px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 24px;
+  &-title {
+    font-size: $body-font-size;
+  }
+  &-image {
+    width: 100%;
+    border-radius: 8px;
+    @media (min-width: $tablet-lower-breakpoint) {
+      max-width: $mobile-upper-breakpoint;
+    }
+  }
+}
+</style>
