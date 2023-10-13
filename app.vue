3c3b1f55adec85fb9082d576fa9268b000a68d24
Diego Reyes <diegorele13@gmail.com>
2023-10-13 15:26:53 -0500

    get the videos

--- app.vue
+++ app.vue
@@ -1,7 +1,20 @@
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import useClubStore from './store/clubStore';
+
+const clubStore = useClubStore();
+
+onMounted(() => {
+  if (!clubStore.videos) {
+    clubStore.getVideos();
+  }
+})
+</script>
+
 <template>
-  <div class="app-container">
+  <div class=“app-container”>
     <NuxtLayout>
-      <div class="app-container-view">
+      <div class=“app-container-view”>
         <NuxtPage   />
       </div>
     </NuxtLayout>
@@ -9,15 +22,14 @@
 </template>
 
 <style lang="scss">
-
 .app-container {
-	min-height: 100vh;
-	width: 100%;
-	display: flex;
-	flex-direction: column;
+  min-height: 100vh;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
   background-color: $dark-purple;
-	&-view{
-		flex: 1;
-	}
+  &-view{
+    flex: 1;
+  }
 }
 </style>
