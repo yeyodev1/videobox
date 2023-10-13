a440c0c963c9bf8b8259b8b19a54e3cd2ef2f380
Diego Reyes <diegorele13@gmail.com>
2023-10-13 18:29:32 -0500

    done: video parser

---
+++ utils/videoParser.ts
@@ -0,0 +1,19 @@
+import { ParsedVideo, VideoInput } from "~/typings/VideoTypes";
+
+export function parseVideoName({name, directLink}: VideoInput) : ParsedVideo | null {
+  const match = name.match(/(\d{4}-\d{2}-\d{2}) (\d{2}-\d{2}-\d{2}) CAM (\d+).mp4/);
+
+  if (match) {
+    const [ , date, time, cam] = match;
+    const formattedTime = time.replace(/-/g, ":");
+    
+    return {
+      date: date,
+      time: formattedTime,
+      field: `CAM ${cam}`,
+      link: directLink
+    };
+  } else {
+    return null
+  }
+}
