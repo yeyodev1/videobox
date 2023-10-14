576bc6e8aa8dfb4154f2af92e2cba0cf6df100d5
Diego Reyes <diegorele13@gmail.com>
2023-10-13 19:03:57 -0500

    done: store

--- store/clubStore.ts
+++ store/clubStore.ts
@@ -6,28 +6,34 @@ import type { VideoType } from "~/typings/VideoTypes";
 
 const videoService = new VideoService();
 interface RootState {
-  club: {
-		name: string;
-		image: string
-	},
-  videos: null | VideoType[]
+  clubs: Club[];
 }
 
 const useClubStore = defineStore("useClubStore", {
   state: (): RootState => ({
-    club: {
-      name: 'el club Diego, hazlo y que lo lea',
-      image: 'https://i.pinimg.com/236x/c5/3d/53/c53d537f7e8698a52ead8b02bfaba095.jpg'
-    },
-    videos: null
+    clubs: [
+      {  
+      image: 'https://i.pinimg.com/236x/c5/3d/53/c53d537f7e8698a52ead8b02bfaba095.jpg',
+      name: 'el club Diego',
+      id: 'club-diego',
+      sports: [
+          {
+            name: 'padel',
+            image: 'https://i.pinimg.com/236x/c5/3d/53/c53d537f7e8698a52ead8b02bfaba095.jpg',
+            id: 'padel-1',
+            videos: []
+          }
+        ],
+      },
+    ]
   }),
 
   actions: {
     async getVideos(): Promise<void> {
       try {
         const response = await videoService.getVideos();
-        this.videos = response.data?.map(video => parseVideoName(video)).filter(Boolean) as VideoType;
-        console.log(this.videos);
+        this.clubs[0].sports[0].videos = response.data?.map(video => parseVideoName(video)).filter(Boolean) as VideoType;
+        console.log(this.clubs[0].sports[0].videos);
       } catch (error) {
         console.log(error);
       }
