bf457ad3871891b09b6e2e8ad910267557ffafe8
Luis Alberto Reyes Lema <lualreye@Luiss-MacBook-Pro.local>
2023-10-17 14:13:08 -0500

    FIX: video service

--- services/Videos/Videos.ts
+++ services/Videos/Videos.ts
@@ -3,7 +3,7 @@ import APIBase from '../Base';
 import type { VideoType } from '~/typings/VideoTypes';
 class VideoService extends APIBase {
   async getVideos(): Promise<AxiosResponse<VideoType[] | null>> {
-    return this.get('/videos')
+    return this.get('videos')
   }
 }
 export default VideoService;
