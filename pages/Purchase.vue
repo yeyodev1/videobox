116d279e4644879411b882b810a8bd08465c7711
Diego Reyes <diegorele13@gmail.com>
2023-10-19 18:01:13 -0500

    add: links to social media

--- pages/Purchase.vue
+++ pages/Purchase.vue
@@ -1,15 +1,38 @@
+<script setup>
+const copyToClipboard = (event) => {
+  const range = document.createRange();
+  range.selectNode(event.target);
+  window.getSelection().removeAllRanges();
+  window.getSelection().addRange(range);
+  document.execCommand('copy');
+  window.getSelection().removeAllRanges();
+  alert('Número copiado al portapapeles');
+};
+</script>
+
 <template>
   <div class="section">
     <p class="section-title">
       ¡Gracias por enviarnos tus datos!
     </p>
     <p class="section-title">
-      Por favor, YAPEAR S/ 10 al 906894857 ¡Envíanos la captura de pantalla del pago por WhatsApp al mismo número, y te enviaremos tu video! <a href="/" class="number">+593 349494949</a>
+      Por favor, YAPEAR S/ 10 al <span class="number" @click="copyToClipboard"> 906894857</span> ¡Envíanos la captura de pantalla del pago por WhatsApp al mismo número, y te enviaremos tu video! <a href="https://api.whatsapp.com/send/?phone=51906894857&text=Estoy+interesado+en+adquirir+un+video&type=phone_number&app_absent=0" class="number">+51 906 894 857</a>
     </p>
     <img 
       src="@/assets/qr-code.jpg" 
       alt="Código de pago" 
       class="section-image">
+    <p class="section-title">
+      Cualquier consulta, escribenos al WhatsApp o Instagram!
+    </p>
+    <p class="section-icons">
+      <a href="https://www.instagram.com/videobox.pe?igshid=MjEwN2IyYWYwYw%3D%3D">
+        <i class="fa-brands fa-instagram" />
+      </a>
+      <a href="https://api.whatsapp.com/send/?phone=51906894857&text=Estoy+interesado+en+adquirir+un+video&type=phone_number&app_absent=0">
+      <i class="fa-brands fa-whatsapp" />
+      </a>
+    </p>
   </div>
 </template>
 
@@ -26,6 +49,7 @@
     text-align: center;
     .number {
       text-decoration: underline;
+      cursor: pointer;
     }
   }
   &-image {
@@ -34,6 +58,12 @@
     @media (min-width: $mobile-lower-breakpoint) {
       max-width: $mobile-lower-breakpoint;
     }
+  }
+  &-icons {
+    font-size: $h2-font-size;
+    text-align: center;
+    display: flex;
+    gap: 16px;
   }
 }
 </style>
