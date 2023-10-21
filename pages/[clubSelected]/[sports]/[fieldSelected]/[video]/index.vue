8e5e32d16133de7d4193e87c9cd7d8b1d6f344e4
Diego Reyes <diegorele13@gmail.com>
2023-10-20 22:34:01 -0500

    set: condition

--- pages/[clubSelected]/[sports]/[fieldSelected]/[video]/index.vue
+++ pages/[clubSelected]/[sports]/[fieldSelected]/[video]/index.vue
@@ -8,7 +8,7 @@ import VideoCrazy from '@/components/Video.vue';
       aqui ira el video
     </p>
     <VideoCrazy
-      video-url="https://v3.cdnpk.net/videvo_files/video/free/2019-11/large_watermarked/190301_1_25_11_preview.mp4"
+      video-url='https://storage.googleapis.com/videbox-bucket/video2023-08-28%2016-30-06%20CAM%201.mp4'
       :no-show-controls="true"
       :options="false" />
   </div>
