a73f2262e250374d2632b658f9195dcbf38cd942
Diego Reyes <diegorele13@gmail.com>
2023-10-21 19:02:07 -0500

    link of leons

--- pages/[clubSelected]/[sports]/[fieldSelected]/[video]/index.vue
+++ pages/[clubSelected]/[sports]/[fieldSelected]/[video]/index.vue
@@ -1,5 +1,28 @@
 <script setup>
+import axios from 'axios';
+
 import VideoCrazy from '@/components/Video.vue';
+
+const videoUrl = ref(null);
+
+const fetchVideo = async () => {
+  try {
+    const response = await axios.get('https://storage.googleapis.com/videbox-bucket/video2023-08-28%2016-30-06%20CAM%201.mp4');
+
+    if(!response.ok) {
+      throw new Error (`Network response was not ok: ${response.statusText}`)
+    };
+    console.log(response)
+    const blob = await response.blob();
+    videoUrl.value = URL.createObjectURL(blob);
+  } catch (error){
+    console.log('hubo un problema con el fetcheo', error)
+  }
+}
+
+onMounted(() => {
+  fetchVideo();
+})
 </script>
 
 <!-- video-url='https://storage.googleapis.com/videbox-bucket/video2023-08-28%2016-30-06%20CAM%201.mp4' -->
@@ -9,7 +32,7 @@ import VideoCrazy from '@/components/Video.vue';
       aqui ira el video
     </p>
     <VideoCrazy
-      video-url="https://v3.cdnpk.net/videvo_files/video/free/2019-11/large_watermarked/190301_1_25_11_preview.mp4"
+      :video-url="'https://v3.cdnpk.net/videvo_files/video/free/2019-11/large_watermarked/190301_1_25_11_preview.mp4'"
       :no-show-controls="true"
       :options="false" />
   </div>
