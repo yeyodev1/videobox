4b39554b27ff5812e144af27d837a0b03c7653f5
Diego Reyes <diegorele13@gmail.com>
2023-12-03 13:17:01 -0500

    refact new parsed video

--- utils/videoParser.ts
+++ utils/videoParser.ts
@@ -1,47 +1,28 @@
 import { ParsedVideo, VideoInput } from "~/typings/VideoTypes";
 
-export function parseVideoName({name, fileId, _id, url}: VideoInput) : ParsedVideo | null {
-  const match = name.match(/(\d{4}-\d{2}-\d{2}) (\d{2}-\d{2}-\d{2}) CAM (\d+).mp4/);
+export function parseVideoName({ name, fileId, _id, url }: VideoInput): ParsedVideo | null {
+  const match = name.match(/Cancha (\d+) CAM (\d+) (\w+) (\w+) (\d{4}-\d{2}-\d{2}) (\d{2}-\d{2}-\d{2})\.mp4/);
 
   if (match) {
-    const [ , date, time, cam] = match;
-    const formattedTime = time.replace(/-/g, ":");
+    const [ , fieldNumber, camNumber, clubName, sportName, date, time] = match;
+    const formattedDate = date.replace(/-/g, '/');
+    const formattedTime = time.replace(/-/g, ':');
 
     return {
-      date: date,
+      field: `Cancha ${fieldNumber}`,
+      cam: `CAM ${camNumber}`,
+      club: clubName,
+      sport: sportName,
+      date: formattedDate,
       time: formattedTime,
-      field: `Cancha ${cam}`,
-      fileId: fileId,
-      id: _id,
-      url: url
+      fileId: fileId, 
+      id: _id, 
+      url: url 
     };
   } else {
-    return null
+    return null;
   }
 }
-
-// export function parseVideoName({name, fileId, _id, url}: VideoInput) : ParsedVideo | null {
-//   const match = name.match(/Cancha (\d+) CAM (\d+) (\w+) (\w+) (\d{4}-\d{2}-\d{2}) (\d{2}-\d{2}-\d{2}).mp4/);
-
-//   if (match) {
-//     const [ , cancha, cam, clubName, sportName, date, time] = match;
-//     const formattedTime = time.replace(/-/g, ":");
-
-//     return {
-//       date: date,
-//       time: formattedTime,
-//       field: `Cancha ${cancha}`, 
-//       clubName: clubName,       
-//       sportName: sportName,     
-//       fileId: fileId,
-//       id: _id,
-//       url: url
-//     };
-//   } else {
-//     return null;
-//   }
-// }
-
 
 export function filterVideosByField(videos: ParsedVideo[], selectedField: string): ParsedVideo[] {
   return videos.filter(video => video.field === selectedField);
