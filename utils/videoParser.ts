99fff862cc6c5812150c4ae17cb97705854243bf
Luis Alberto Reyes Lema <lualreye@Luiss-MacBook-Pro.local>
2023-12-20 05:57:45 -0500

    FEAT: new organization on videos

--- utils/videoParser.ts
+++ utils/videoParser.ts
@@ -1,18 +1,22 @@
 import { ParsedVideo, VideoInput } from "~/typings/VideoTypes";
 
 export function parseVideoName({ name, fileId, _id, url }: VideoInput): ParsedVideo | null {
-  const match = name.match(/Cancha (\d+) CAM (\d+) (\w+) (\w+) (\d{4}-\d{2}-\d{2}) (\d{2}-\d{2}-\d{2})\.mp4/);
+  // Actualiza la expresión regular para coincidir con el nuevo formato
+  const match = name.match(/Cancha (\d+) CAM (\d+) (\w+) (\w+) (\d{2}-\d{2}) (\d{2}-\d{2}-\d{2})\.mp4/);
 
   if (match) {
-    const [ , fieldNumber, camNumber, clubName, sportName, date, time] = match;
-    const formattedDate = date.replace(/-/g, '/');
+    const [ , fieldNumber, camNumber, clubAcronym, sportAcronym, date, time] = match;
+    const formattedDate = `20${date.replace(/-/g, '/')}`; // Asume que el año es 20xx
     const formattedTime = time.replace(/-/g, ':');
+
+    const getClubName = clubAcronym === 'DC' ? 'Deporcentro' : 'One Padel';
+    const getSportName = sportAcronym === 'FUT' ? 'Futbol' : 'Paddle';
 
     return {
       field: `Cancha ${fieldNumber}`,
       cam: `CAM ${camNumber}`,
-      club: clubName,
-      sport: sportName,
+      club: getClubName,
+      sport: getSportName,
       date: formattedDate,
       time: formattedTime,
       fileId: fileId, 
@@ -23,6 +27,7 @@ export function parseVideoName({ name, fileId, _id, url }: VideoInput): ParsedVi
     return null;
   }
 }
+
 
 export function filterVideosByField(videos: ParsedVideo[], selectedField: string): ParsedVideo[] {
   return videos.filter(video => video.field === selectedField);
